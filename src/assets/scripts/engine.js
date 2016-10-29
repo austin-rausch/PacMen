a11ec6972c2b10d08bb35ce5c28c27495955e5c0
Chris Andrejewski <christopher.andrejewski@gmail.com>
2016-10-29 15:46:02 -0400

    fixed game

--- src/assets/scripts/engine.js
+++ src/assets/scripts/engine.js
@@ -1,18 +1,7 @@
-// The base framework for this game was written for Phaser v2.2.2 by Richard Davey (@photonstorm)
-// The game logic fails with the most recent rendition of Phaser(v2.6.2)
-// Modifications are made to allow for the use of the most recent version.
 const {Phaser} = window;
 
-const debugOn = true;
-
-function debug (s) {
-  if (debugOn) {
-    console.log(s);
-  }
-}
-
-export default class PacMan {
-  constructor() {
+export default class Pacman {
+  constructor () {
     this.game = new Phaser.Game(448, 496, Phaser.AUTO);
     this.game.state.add('Game', this, true);
 
@@ -24,7 +13,7 @@ export default class PacMan {
     this.gridsize = 16;
 
     this.speed = 150;
-    this.threshold = 8;
+    this.threshold = 3;
 
     this.marker = new Phaser.Point();
     this.turnPoint = new Phaser.Point();
@@ -36,7 +25,7 @@ export default class PacMan {
     this.turning = Phaser.NONE;
   }
 
-  init() {
+  init () {
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVertically = true;
@@ -46,15 +35,15 @@ export default class PacMan {
     this.physics.startSystem(Phaser.Physics.ARCADE);
   }
 
-  preload() {
-        // Graphics: (C)opyright Namco
+  preload () {
+    //  Needless to say, graphics (C)opyright Namco
     this.load.image('dot', '/sprites/dot.png');
     this.load.image('tiles', '/sprites/pacman-tiles.png');
     this.load.spritesheet('pacman', '/sprites/pacman.png', 32, 32);
     this.load.tilemap('map', '/sprites/pacman-map.json', null, Phaser.Tilemap.TILED_JSON);
   }
 
-  create() {
+  create () {
     this.map = this.add.tilemap('map');
     this.map.addTilesetImage('pacman-tiles', 'tiles');
 
@@ -69,13 +58,10 @@ export default class PacMan {
     this.dots.setAll('y', 6, false, false, 1);
 
         //  Pacman should collide with everything except the safe tile
-    this.map.setCollisionByExclusion([], true, this.layer);
-    this.map.setCollision(this.safetile, false, this.layer);
+    this.map.setCollisionByExclusion([this.safetile], true, this.layer);
 
         //  Position Pacman at grid location 14x17 (the +8 accounts for his anchor)
     this.pacman = this.add.sprite((14 * 16) + 8, (17 * 16) + 8, 'pacman', 0);
-    this.pacman.x = (14 * 16) + 8;
-    this.pacman.y = (17 * 16) + 8;
     this.pacman.anchor.set(0.5);
     this.pacman.animations.add('munch', [0, 1, 2, 1], 20, true);
 
@@ -89,78 +75,48 @@ export default class PacMan {
   }
 
   checkKeys() {
-    if (this.cursors.left.isDown && this.current !== Phaser.LEFT) {
-      debug('Sending Phaser.LEFT to checkDirection()');
-      this.checkDirection(Phaser.LEFT);
-    } else if (this.cursors.right.isDown && this.current !== Phaser.RIGHT) {
-      debug('Sending Phaser.RIGHT to checkDirection()');
-      this.checkDirection(Phaser.RIGHT);
-    } else if (this.cursors.up.isDown && this.current !== Phaser.UP) {
-      debug('Sending Phaser.UP to checkDirection()');
-      this.checkDirection(Phaser.UP);
-    } else if (this.cursors.down.isDown && this.current !== Phaser.DOWN) {
-      debug('Sending Phaser.DOWN to checkDirection()');
-      this.checkDirection(Phaser.DOWN);
-    } else {
-      //  This forces them to hold the key down to turn the corner
-      debug('#set this.turning to Phaser.NONE');
-      this.turning = Phaser.NONE;
+    const {LEFT, RIGHT, UP, DOWN} = Phaser;
+    const {left, right, up, down} = this.cursors;
+    if (left.isDown) return this.tryTurn(LEFT);
+    if (right.isDown) return this.tryTurn(RIGHT);
+    if (up.isDown) return this.tryTurn(UP);
+    if (down.isDown) return this.tryTurn(DOWN);
+  }
+
+  tryTurn(direction) {
+    if (this.current === direction) return;
+    const nextTile = this.directions[direction];
+    const isNextTileSafe = nextTile && nextTile.index === this.safetile;
+    if (isNextTileSafe) {
+      const reverse = this.current === this.opposites[direction];
+      if (reverse) {
+        // move immediately
+        this.move(direction);
+      } else {
+        // promise to turn at tile center point
+        this.turning = direction;
+        this.turnPoint.x = (this.marker.x * this.gridsize) + (this.gridsize / 2);
+        this.turnPoint.y = (this.marker.y * this.gridsize) + (this.gridsize / 2);
+      }
     }
   }
 
-  // Checks the corresponding direction (up down left or right) to see if it is
-  // a valid tile to move to
-  checkDirection(turnTo) {
-    // Sprite is already set to turn in the provided direction
-    if (this.turning === turnTo) {
-      debug('this.turning === turnTo');
-      return;
-    } else if (this.directions[turnTo] === null) {
-      // No tile to move to in direction turnTo
-      debug('this.directions[turnTo] === null');
-      return;
-    } else if (this.directions[turnTo].index !== this.safetile) {
-      // The target tile to move to is not a safe tile (cant move to that tile)
-      debug('this.directions[turnTo].index !== this.safetile');
-      return;
-    }
-
-    //  Check if they want to turn around and can
-    if (this.current === this.opposites[turnTo]) {
-      debug(`Calling this.move(turnTo) with value of ${  turnTo}`);
-      this.move(turnTo);
-    } else {
-      debug(`Set this.turning in checkDirection() = ${  turnTo}`);
-      this.turning = turnTo;
-
-      this.turnPoint.x = (this.marker.x * this.gridsize) + (this.gridsize / 2);
-      this.turnPoint.y = (this.marker.y * this.gridsize) + (this.gridsize / 2);
-      debug(`this.turnPoint.x set to: ${  this.turnPoint.x}`);
-      debug(`this.turnPoint.y set to: ${  this.turnPoint.y}`);
-    }
-  }
-
-  turn() {
+  turn () {
     const cx = Math.floor(this.pacman.x);
     const cy = Math.floor(this.pacman.y);
-
-    debug('IN TURNING FUNCTION');
-    debug(`Math.floor(this.pacman.x) = ${  cx}`);
-    debug(`Math.floor(this.pacman.x) = ${  cy}`);
-
-    debug(`this.turnPoint.x = ${  this.turnPoint.x}`);
-    debug(`this.turnPoint.y = ${  this.turnPoint.y}`);
-
-    debug(`Difference must be less than: ${  this.threshold}`);
 
     // This needs a threshold, because at high speeds you can't turn
     // because the coordinates skip past
     const xChange = this.math.fuzzyEqual(cx, this.turnPoint.x, this.threshold);
     const yChange = this.math.fuzzyEqual(cy, this.turnPoint.y, this.threshold);
-    if (!(xChange && yChange)) {
-      debug('Returning false');
-      return false;
+    const canTurn = xChange && yChange;
+    if (!canTurn) {
+      return;
     }
+
+        //  Grid align before turning
+    this.pacman.x = this.turnPoint.x;
+    this.pacman.y = this.turnPoint.y;
 
     this.pacman.body.reset(this.turnPoint.x, this.turnPoint.y);
 
@@ -171,7 +127,7 @@ export default class PacMan {
     return true;
   }
 
-  move(direction) {
+  move (direction) {
     let speed = this.speed;
 
     if (direction === Phaser.LEFT || direction === Phaser.UP) {
@@ -199,7 +155,7 @@ export default class PacMan {
     this.current = direction;
   }
 
-  eatDot(pacman, dot) {
+  eatDot (pacman, dot) {
     dot.kill();
 
     if (this.dots.total === 0) {
@@ -207,20 +163,22 @@ export default class PacMan {
     }
   }
 
-  update() {
+  update () {
     this.physics.arcade.collide(this.pacman, this.layer);
     this.physics.arcade.overlap(this.pacman, this.dots, this.eatDot, null, this);
 
-    // const x = this.math.snapToFloor(Math.floor(this.pacman.x), this.gridsize) / this.gridsize;
-    // const y = this.math.snapToFloor(Math.floor(this.pacman.y), this.gridsize) / this.gridsize;
     this.marker.x = this.math.snapToFloor(Math.floor(this.pacman.x), this.gridsize) / this.gridsize;
     this.marker.y = this.math.snapToFloor(Math.floor(this.pacman.y), this.gridsize) / this.gridsize;
 
-    //  Update our grid sensors
-    this.directions[1] = this.map.getTileLeft(this.layer.index, this.marker.x, this.marker.y);
-    this.directions[2] = this.map.getTileRight(this.layer.index, this.marker.x, this.marker.y);
-    this.directions[3] = this.map.getTileAbove(this.layer.index, this.marker.x, this.marker.y);
-    this.directions[4] = this.map.getTileBelow(this.layer.index, this.marker.x, this.marker.y);
+    {
+      //  Update our grid sensors
+      const {x, y} = this.marker;
+      const {index} = this.layer;
+      this.directions[Phaser.LEFT] = this.map.getTileLeft(index, x, y);
+      this.directions[Phaser.RIGHT] = this.map.getTileRight(index, x, y);
+      this.directions[Phaser.UP] = this.map.getTileAbove(index, x, y);
+      this.directions[Phaser.DOWN] = this.map.getTileBelow(index, x, y);
+    }
 
     this.checkKeys();
 
